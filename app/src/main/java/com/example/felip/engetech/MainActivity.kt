308138605101fd752c0363d126990b5c357d1390
fegonc <felipeepires.95@gmail.com>
2018-07-30 21:51:07 -0300

    This is a new commit

---
+++ app/src/main/java/com/example/felip/engetech/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.felip.engetech
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.foto.*
+
+class MainActivity : AppCompatActivity() {
+
+    fun View.chamaTela1() {
+        setContentView(R.layout.tela1)
+    }
+
+    fun View.chamaTela2() {
+        setContentView(R.layout.tela2)
+    }
+
+    fun View.chamaFoto1() {
+        setContentView(R.layout.foto)
+
+        tirarFoto.setOnClickListener {
+            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (callCameraIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(callCameraIntent, CAMERA_REQUEST_CODE)
+            }
+
+        }
+    }
+
+
+
+    fun View.chamaFoto2() {
+        setContentView(R.layout.foto)
+
+        tirarFoto.setOnClickListener {
+            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (callCameraIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(callCameraIntent, CAMERA_REQUEST_CODE)
+            }
+
+        }
+    }
+
+
+
+
+    val CAMERA_REQUEST_CODE = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    lugarDaFoto.setImageBitmap(data.extras.get("data") as Bitmap)
+                }
+            }
+            else -> {
+                Toast.makeText( this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+
+    }
+
+
